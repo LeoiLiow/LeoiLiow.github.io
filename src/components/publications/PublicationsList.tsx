81861d1c283355b43b5862d7210d63ec829430e4
LeoiLiow <leiliao1999@gmail.com>
2026-08-11 21:51:25 -0400

    Update PublicationsList.tsx

--- src/components/publications/PublicationsList.tsx
+++ src/components/publications/PublicationsList.tsx
@@ -206,7 +206,9 @@ export default function PublicationsList({ config, publications, embedded = fals
                                             <Image
                                                 src={`/papers/${pub.preview}`}
                                                 alt={pub.title}
-                                                fill
+                                                width={1200} 
+                                                height={600}
+                                                className="w-full h-auto"
                                                 className="object-contain"
                                                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                                             />
